9fa478e67678627a91de5fd09f68243d2230dd8f
Mathieu Mbuy <evamath65@gmail.com>
2026-04-24 15:04:23 -0400

    Add PWA mobile install support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,20 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Source+Code+Pro&display=swap"
           rel="stylesheet"
         />
+         {/* PWA Support */}
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="theme-color" content="#0f172a" />
+  <link rel="apple-touch-icon" href="/icon-192.png" />
+
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta
+    name="apple-mobile-web-app-title"
+    content="ManageWiseMD"
+  />
+  <meta
+    name="apple-mobile-web-app-status-bar-style"
+    content="default"
+  />
       </head>
       <body className="min-h-screen w-full overflow-x-hidden font-body antialiased" suppressHydrationWarning>
         <Providers>
